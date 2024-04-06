318bdc01c9e102f2e2c7d45cfee0587532a9ca98
butterriolu <derpvin12@gmail.com>
2024-04-06 12:23:49 -0500

    Add solver to problem

--- src/service/solution-service.ts
+++ src/service/solution-service.ts
@@ -5,7 +5,7 @@ import { SolutionAttributes } from '../models/Solution';
 import { throwUnexpectedAsHttpError } from '../utils/errors';
 import { createPaginationPipelineStages, getTextLength, toObjectId } from '../utils/mongoose';
 import { CreateSolutionRequestBody, ProblemSolutions, SolutionFilterable, SolutionSortable } from '../types/solutions';
-import { PROBLEM_FILTERABLE_KEYS, SOLUTION_FILTERABLE_KEYS, SOLUTION_SORTABLES } from '../constants';
+import { PROBLEM_FILTERABLE_KEYS, SOLUTION_FILTERABLE_KEYS } from '../constants';
 import { ProblemFilterable } from '../types/problem';
 
 /**
@@ -19,7 +19,7 @@ async function create(
   const session = await Models.Solution.startSession();
   session.startTransaction();
   try {
-    // Userid should already exis
+    // Userid should already exist
     const userIsVerified = await Models.User.exists({
       _id: data.user_id,
       verified: true,
@@ -30,26 +30,33 @@ async function create(
     }
 
     // Update the problem
-    const updatedProblemFields: Partial<ProblemAttributes> = {
+    const updatedProblemFields: Omit<ProblemAttributes, '_id' | 'solved_by' | 'created_at'> = {
       id: data.problem_id,
       title: data.problem_title,
       url: data.problem_url,
       difficulty: data.problem_difficulty,
-
       // deal with empty alt names and embed css
       description: data.problem_description.replaceAll(`alt=""`, `alt="visual" style="height:auto; max-width:100%;"`),
       tags: data.problem_tags,
     };
 
+    // update problem details
     const updatedProblem: ProblemAttributes = await Models.Problem.findOneAndUpdate(
       { id: updatedProblemFields.id },
       updatedProblemFields,
       { upsert: true, new: true, session }
     );
 
+    // add solved user
+    const updatedProblemWithSolver = await Models.Problem.findOneAndUpdate(
+      { _id: updatedProblem._id },
+      { $addToSet: { solved_by: data.user_id } },
+      { upsert: true, new: true, session }
+    );
+
     const solutionData: Omit<SolutionAttributes, '_id'> = {
       user_id: toObjectId(data.user_id),
-      problem_id: updatedProblem._id,
+      problem_id: updatedProblemWithSolver._id,
       solution_language: data.solution_language,
       solution_code: data.solution_code,
       solution_length: getTextLength(data.solution_code),
@@ -171,8 +178,6 @@ async function findUserSolutions(
           return { $match: { [key]: queryParams[key] } };
       }
     });
-
-    console.log(JSON.stringify(preAggregationSolutionFilters));
 
     const sortByMostRecent: PipelineStage = { $sort: { created_at: -1 } };
 
