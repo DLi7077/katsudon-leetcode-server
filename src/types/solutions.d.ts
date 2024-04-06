969078ef06d50b7e38d6743dbb19e4fa1d252fd1
butterriolu <derpvin12@gmail.com>
2024-04-06 01:38:42 -0500

    Add find user solutions route

---
+++ src/types/solutions.d.ts
@@ -0,0 +1,23 @@
+import { ProblemAttributes } from '../models/Problem';
+import { SolutionAttributes } from '../models/Solution';
+
+export interface CreateSolutionRequestBody {
+  user_id: string;
+  memory_usage_mb: number;
+  problem_description: string;
+  problem_difficulty: string;
+  problem_id: number;
+  problem_tags: string[];
+  problem_title: string;
+  problem_url: string;
+  runtime_ms: number;
+  failed?: boolean;
+  error?: string;
+  solution_code: string;
+  solution_language: string;
+}
+
+export interface ProblemSolutions {
+  problem: ProblemAttributes;
+  solutions: SolutionAttributes[];
+}
